5b1fa4f35e5e96836329938e9a252eecf5cd5074
Sagar Raulo <sagarraulo@gmail.com>
2023-09-10 17:03:57 +0530

    post generation

--- post.js
+++ post.js
@@ -4,7 +4,7 @@ let mainPost = [{
     "userName": "ResidentsMedical",
     "userId": "@residentsmed",
     "dots": "assets/img/dots.svg",
-    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US",
+    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US 1",
     "image": "https://pbs.twimg.com/media/FdKEfYcXgAI9H7a?format=jpg&name=small",
     "actions": [ 
     {"image": "assets/img/comments.svg"},
@@ -18,7 +18,7 @@ let mainPost = [{
     "username": "ResidentsMedical",
     "userid": "@residentsmed",
     "dots": "assets/img/dots.svg",
-    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US",
+    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US 2",
     "image": "https://pbs.twimg.com/media/FdKEfYcXgAI9H7a?format=jpg&name=small",
     "actions": [ {"image": "assets/img/comments.svg"},
     {"image": "assets/img/thread.svg"},
@@ -31,7 +31,7 @@ let mainPost = [{
     "username": "ResidentsMedical",
     "userid": "@residentsmed",
     "dots": "assets/img/dots.svg",
-    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US",
+    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US 3",
     "image": "https://pbs.twimg.com/media/FdKEfYcXgAI9H7a?format=jpg&name=small",
     "actions": [
         {"image": "assets/img/comments.svg"},
@@ -46,7 +46,7 @@ let mainPost = [{
     "username": "ResidentsMedical",
     "userid": "@residentsmed",
     "dots": "assets/img/dots.svg",
-    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US",
+    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US 4",
     "image": "https://pbs.twimg.com/media/FdKEfYcXgAI9H7a?format=jpg&name=small",
     "actions": [
         {"image": "assets/img/comments.svg"},
@@ -60,7 +60,7 @@ let mainPost = [{
     "username": "ResidentsMedical",
     "userid": "@residentsmed",
     "dots": "assets/img/dots.svg",
-    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US",
+    "description": "India a USD 2000 per capita economy oil prices breaking our back, says Foreign Minister S Jayashankar in US 5",
     "image": "https://pbs.twimg.com/media/FdKEfYcXgAI9H7a?format=jpg&name=small",
     "actions": [
         {"image": "assets/img/comments.svg"},
@@ -74,9 +74,11 @@ console.log(document);
 console.log(document.getElementById("content"));
 const content = document.getElementById("content");
 content.className = "post";
+for(let y of mainPost) {
+
 
 const postAvatar = document.createElement("img");
-postAvatar.src = mainPost[0].postAvatar;
+postAvatar.src = y.postAvatar;
 
 const div0 = document.createElement("div");
 div0.appendChild(postAvatar);
@@ -89,7 +91,7 @@ div1.className = "post";
 
 
 const dots = document.createElement("img");
-dots.src = mainPost[0].dots;
+dots.src = y.dots;
 
 
 const div2 =  document.createElement("div");
@@ -97,11 +99,11 @@ div2.appendChild(dots);
 div2.className = "dots";
 
 const bold = document.createElement("b");
-const boldText = document.createTextNode(mainPost[0].userName);
+const boldText = document.createTextNode(y.userName);
 bold.appendChild(boldText);
 
 const userId = document.createElement("h3");
-const userIdText = document.createTextNode(mainPost[0].userid);
+const userIdText = document.createTextNode(y.userid);
 userId.appendChild(bold);
 userId.appendChild(userIdText);
 userId.appendChild(div2);
@@ -111,7 +113,7 @@ div3.appendChild(userId);
 div3.className = "post__headertext";
 
 const description = document.createElement("p");
-const descriptionText = document.createTextNode(mainPost[0].description);
+const descriptionText = document.createTextNode(y.description);
 description.appendChild(descriptionText);
 
 const divdescription = document.createElement("div");
@@ -132,56 +134,118 @@ content.appendChild(div1);
 
 
 const image = document.createElement("img");
-image.src = mainPost[0].image;
+image.src = y.image;
 
 const div6 =  document.createElement("div");
 div6.appendChild(image);
 div6.className = "post__image";
 div5.appendChild(div6);
-
-
-
-const comments = document.createElement("img");
-comments.src = mainPost[0].actions[0].image;
-
-const div7 = document.createElement("div");
-div7.appendChild(comments);
-div7.className = "post__footer";
-div5.appendChild(div7);
-
-
-
-const thread = document.createElement("img");
-thread.src = mainPost[0].actions[1].image;
-
-const div8 = document.createElement("div");
-div8.appendChild(thread);
-div8.className = "post__footer";
-div5.appendChild(div8);
-
-
-const upload = document.createElement("img");
-upload.src = mainPost[0].actions[2].image;
-
-const div9 = document.createElement("div");
-div9.appendChild(upload);
-div9.className = "post__footer";
-div5.appendChild(div9);
-
-
-
-const like = document.createElement("img");
-like.src = mainPost[0].actions[3].image;
-
-const div10 = document.createElement("div");
-div10.appendChild(like);
-div10.className = "post__footer";
-div5.appendChild(div10);
-
-let text = "";
-for(let i=0; i < actions.length; i++){
-    text += actions[i];
+ 
+for(let x in mainPost[0].actions) {
+    let image = document.createElement("img");
+    image.src = mainPost[0].actions[x].image;
+    let div11 = document.createElement("div");
+    div11.appendChild(image);
+    div11.className = "post__footer";
+    div5.appendChild(div11);
 }
+}
+
+
+
+// const comments = document.createElement("img");
+// comments.src = mainPost[0].actions[0].image;
+
+// const div7 = document.createElement("div");
+// div7.appendChild(comments);
+// div7.className = "post__footer";
+// div5.appendChild(div7);
+
+
+
+// const thread = document.createElement("img");
+// thread.src = mainPost[0].actions[1].image;
+
+// const div8 = document.createElement("div");
+// div8.appendChild(thread);
+// div8.className = "post__footer";
+// div5.appendChild(div8);
+
+
+// const upload = document.createElement("img");
+// upload.src = mainPost[0].actions[2].image;
+
+// const div9 = document.createElement("div");
+// div9.appendChild(upload);
+// div9.className = "post__footer";
+// div5.appendChild(div9);
+
+
+
+// const like = document.createElement("img");
+// like.src = mainPost[0].actions[3].image;
+
+// const div10 = document.createElement("div");
+// div10.appendChild(like);
+// div10.className = "post__footer";
+// div5.appendChild(div10);
+
+// let i;
+// for(let x of mainPost[0].actions) {
+//     let image = document.createElement("img");
+//     let imageSource = mainPost[0].actions[i];
+//     console.log(mainPost[0].actions[i]);
+
+
+// }
+
+// ((this is normal for loop))
+
+// for(i=0; i<=3;i++){
+//     let image = document.createElement("img");
+//     image.src = mainPost[0].actions[i].image;
+
+//     let div11 = document.createElement("div");
+//     div11.appendChild(image);
+//     div11.className = "post__footer";
+//     div5.appendChild(div11);
+
+// }
+
+// ((this is for of loop))
+
+// for(let x of mainPost[0].actions) {
+//     let image = document.createElement("img");
+//     image.src = x.image;
+
+//     let div11 = document.createElement("div");
+//     div11.appendChild(image);
+//     div11.className = "post__footer";
+//     div5.appendChild(div11);
+
+// }
+
+// ((this is for in loop))
+
+
+
+ 
+//  for(let x in mainPost[0].actions) {
+//     let image = document.createElement("img");
+//     image.src = mainPost[0].actions[x].image;
+//     let div11 = document.createElement("div");
+//     div11.appendChild(image);
+//     div11.className = "post__footer";
+//     div5.appendChild(div11);
+// }
+
+
+
+   
+ 
+   
+ 
+
 
 
 
